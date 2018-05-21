36020da34aa6bafbc332fa705e676e031d4193d7
Edison Neto <ednetoali@gmail.com>
2018-05-21 20:43:55 -0300

    new file:   MANIFEST.in 	new file:   amberis/__init__.py 	new file:   requirements.txt 	new file:   setup.py
    Added files needed to create a python package.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="amberis",
+    version="0.1",
+    author="Edison Neto, Douglas Silva, Douglas Rojas and Kaique juvêncio",
+    author_email="ednetoali@gmail.com",
+    packages=['amberis'],
+    include_package_data=True,
+    install_requires=[
+        'pygame'
+    ],
+)
